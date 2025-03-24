c42f874f600de3a9fa73a9d701359aa188d173f5
Karoid <shj5508@naver.com>
2025-03-24 11:04:25 +0900

    [Feat] Code Section font changed

--- webapp/src/manifest.js
+++ webapp/src/manifest.js
@@ -8,7 +8,7 @@ const manifest = JSON.parse(`
     "homepage_url": "https://github.com/Karoid/mattermost-plugin-rounded-theme",
     "support_url": "https://github.com/Karoid/mattermost-plugin-rounded-theme/issues",
     "icon_path": "assets/rounded-theme.svg",
-    "version": "0.0.1",
+    "version": "0.0.2",
     "min_server_version": "6.2.1",
     "webapp": {
         "bundle_path": "webapp/dist/main.js"
